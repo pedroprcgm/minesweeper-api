5371999965ac89e00903fea25abd96b63e39ccff
Pedro Guerreiro <pedro.prcgm@gmail.com>
2021-05-16 17:22:00 -0300

    Starter project (clean)

---
+++ src/MineSweeper.API/Controllers/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MineSweeper.Services.Host.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GameController : ControllerBase
+    {
+        private readonly ILogger<GameController> _logger;
+
+        public GameController(ILogger<GameController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
