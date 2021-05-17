7686a1b2b5b8b041eb917091558e833bf18fef95
Pedro Guerreiro <pedro.prcgm@gmail.com>
2021-05-16 22:49:26 -0300

    Added swagger to project

--- src/MineSweeper.API/Startup.cs
+++ src/MineSweeper.API/Startup.cs
@@ -3,12 +3,15 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using MineSweeper.Application.Interfaces;
 using MineSweeper.Application.Services;
 using MineSweeper.Domain.Interfaces.Context;
 using MineSweeper.Domain.Interfaces.Repositories;
+using MineSweeper.Infra.Context;
 using MineSweeper.Infra.Repositories;
 using MineSweeper.Infra.UoW;
+using System;
 
 namespace MineSweeper.API
 {
@@ -24,6 +27,27 @@ namespace MineSweeper.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "MineSweeper API",
+                    Description = "Swagger documentation",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Pedro Guerreiro",
+                        Email = "pedro.prcgm@gmail.com",
+                        Url = new Uri("https://www.linkedin.com/in/pedro-guerreiro-b43789101/")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT",
+                        Url = new Uri("https://github.com/pedroprcgm/minesweerper-api/blob/main/LICENSE.md")
+                    }
+                });
+            });
 
             RegisterServices(services);
         }
@@ -41,6 +65,16 @@ namespace MineSweeper.API
 
             app.UseAuthorization();
 
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "swagger/{documentName}/swagger.json";
+            });
+
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "MineSweeper API v1.0");
+            });
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
@@ -49,7 +83,7 @@ namespace MineSweeper.API
 
         private void RegisterServices(IServiceCollection services)
         {
-            services.AddScoped<IGameContext, IGameContext>();
+            services.AddScoped<IMineSweeperContext, MineSweeperContext>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             // Repositories
